49e85a6c05b1b24c8ec20ce702356a16ea44a341
DarkSharpness <2040703891@qq.com>
2024-05-06 14:47:09 +0800

    feat(thread): testing on conditional variable

---
+++ multithread/common.h
@@ -0,0 +1,12 @@
+#pragma once
+// Mutlithread utilities
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+#include <semaphore>
+
+// Basic utilities
+#include <iostream>
+#include <vector>
+#include <format>
---
+++ multithread/cond.cpp
@@ -0,0 +1,40 @@
+#include "common.h"
+
+std::mutex lock;
+std::condition_variable cv;
+
+std::size_t count;
+const std::size_t N = 9;
+
+void consumer() {
+    while (true) {
+        std::unique_lock lock(::lock);
+        while (count == 0) cv.wait(lock);
+        --count;
+        std::cout << std::format("Consumer: {} -> {}\n", count + 1, count);
+        cv.notify_all();
+    }
+}
+
+void producer() {
+    while (true) {
+        std::unique_lock lock(::lock);
+        while (count == N) cv.wait(lock);
+        ++count;
+        std::cout << std::format("Producer: {} -> {}\n", count - 1, count);
+        cv.notify_all();
+    }
+}
+
+signed main(int argc, char **argv) {
+    int m;
+    std::cin >> m;
+    std::vector<std::thread> threads;
+    threads.reserve(2 * m);
+    for (int i = 0; i < m; ++i) {
+        threads.push_back(std::thread(consumer));
+        threads.push_back(std::thread(producer));
+    }
+    for (auto &thread : threads) thread.join();
+    return 0;
+}
