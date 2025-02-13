1bb1fa29f159dc10cd7b2bd87f0d7ef046039f5e
DarkSharpness <2040703891@qq.com>
2025-02-13 14:16:48 +0800

    feat: testing auto-gen op

---
+++ toys/sets/sets.cpp
@@ -0,0 +1,133 @@
+#include "sets.h"
+#include <algorithm>
+#include <cstdint>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <type_traits>
+#include <vector>
+
+enum class key_set : std::uint64_t {
+    KEY0 = 1 << 0,
+    KEY1 = 1 << 1,
+    KEY2 = 1 << 2,
+    KEY3 = 1 << 3,
+};
+
+inline constexpr auto operator~(key_set x) -> key_set {
+    return static_cast<key_set>(~static_cast<std::underlying_type_t<key_set>>(x));
+}
+
+inline constexpr auto operator&(key_set x, key_set y) -> key_set {
+    using U = std::underlying_type_t<key_set>;
+    return static_cast<key_set>(static_cast<U>(x) & static_cast<U>(y));
+}
+
+template <>
+struct set_op_traits<key_set> : std::true_type {
+    static constexpr bool has_and = true;
+    static constexpr bool has_not = true;
+};
+
+namespace std {
+
+template <typename _Tp>
+inline auto operator&(const std::set<_Tp> &x, const std::set<_Tp> &y) -> std::set<_Tp> {
+    std::vector<_Tp> buffer;
+    buffer.reserve(std::min(x.size(), y.size()));
+    std::ranges::set_intersection(
+        x.begin(), x.end(), y.begin(), y.end(), std::back_inserter(buffer)
+    );
+    return std::set<_Tp>{
+        std::make_move_iterator(buffer.begin()), std::make_move_iterator(buffer.end())
+    };
+}
+
+template <typename _Tp>
+inline auto operator^(const std::set<_Tp> &x, const std::set<_Tp> &y) -> std::set<_Tp> {
+    std::vector<_Tp> buffer;
+    buffer.reserve(x.size());
+    std::ranges::set_symmetric_difference(
+        x.begin(), x.end(), y.begin(), y.end(), std::back_inserter(buffer)
+    );
+    return std::set<_Tp>{
+        std::make_move_iterator(buffer.begin()), std::make_move_iterator(buffer.end())
+    };
+}
+
+} // namespace std
+
+auto display_set(const std::set<int> &set) -> void {
+    std::cout << "{ ";
+    for (auto &elem : set)
+        std::cout << elem << ' ';
+    std::cout << "}";
+    std::cout << std::endl;
+}
+
+template <typename _Tp>
+struct set_op_traits<std::set<_Tp>> : std::true_type {
+    static constexpr bool has_and = true;
+    static constexpr bool has_xor = true;
+};
+
+struct custom_set : private std::set<int> {
+    using std::set<int>::set;
+    using std::set<int>::begin;
+    using std::set<int>::end;
+    custom_set(std::set<int> set) : std::set<int>{std::move(set)} {}
+    auto to_base() const -> const std::set<int> & {
+        return *this;
+    }
+    auto to_base() && -> std::set<int> {
+        return std::move(*this);
+    }
+};
+
+inline auto operator|(const custom_set &x, const custom_set &y) -> custom_set {
+    return custom_set{x.to_base() | y.to_base()};
+}
+
+inline auto operator/(const custom_set &x, const custom_set &y) -> custom_set {
+    return custom_set{x.to_base() / y.to_base()};
+}
+
+inline auto operator==(const custom_set &x, const custom_set &y) -> bool {
+    return x.to_base() == y.to_base();
+}
+
+template <>
+struct set_op_traits<custom_set> : std::true_type {
+    static constexpr bool has_or  = true;
+    static constexpr bool has_sub = true;
+};
+
+auto display_set(const custom_set &set) -> void {
+    return display_set(set.to_base());
+}
+
+auto main() -> int {
+    static_assert(
+        (key_set::KEY0 + key_set::KEY1) <= (key_set::KEY0 | key_set::KEY3 / key_set::KEY2)
+    );
+
+    auto set0 = std::set<int>{1, 2};
+    auto set1 = std::set<int>{2, 3};
+    display_set(set0 | set1);
+    display_set(set0 / set1);
+    display_set(set1 / set0);
+
+    auto set2 = custom_set{1, 2};
+    auto set3 = custom_set{2, 3};
+    display_set(set2 & set3);
+    display_set(set2 / set3);
+    display_set(set3 / set2);
+
+    std::cout << std::boolalpha;
+
+    std::cout << (set3 <= set3) << std::endl;
+    std::cout << (set2 >= set3) << std::endl;
+    std::cout << ((set2 & set3) < set3) << std::endl;
+    std::cout << ((set2 / set3) > set2) << std::endl;
+}
---
+++ toys/sets/sets.h
@@ -0,0 +1,245 @@
+#pragma once
+#include <compare>
+#include <concepts>
+#include <stdexcept>
+#include <type_traits>
+
+inline namespace set_operation {
+
+template <typename _Tp>
+struct set_op_traits : std::false_type {};
+
+template <typename _Tp>
+concept set_like = set_op_traits<_Tp>::value;
+
+namespace __detail {
+
+template <typename _Tp>
+concept set_has_default_and = set_op_traits<_Tp>::has_and;
+
+template <typename _Tp>
+concept set_has_default_or = set_op_traits<_Tp>::has_or;
+
+template <typename _Tp>
+concept set_has_default_xor = set_op_traits<_Tp>::has_xor;
+
+template <typename _Tp>
+concept set_has_default_sub = set_op_traits<_Tp>::has_sub;
+
+template <typename _Tp>
+concept set_has_default_cmp = set_op_traits<_Tp>::has_cmp;
+
+template <typename _Tp>
+concept set_has_default_not = requires(const _Tp &a) {
+    { ~a } -> std::same_as<_Tp>;
+};
+
+template <typename _Tp>
+concept set_has_default_eq = std::equality_comparable<_Tp>;
+
+template <set_like _Tp>
+struct set_op_impl : set_op_traits<_Tp> {
+private:
+    static constexpr auto has_and = set_has_default_and<_Tp>;
+    static constexpr auto has_or  = set_has_default_or<_Tp>;
+    static constexpr auto has_xor = set_has_default_xor<_Tp>;
+    static constexpr auto has_sub = set_has_default_sub<_Tp>;
+    static constexpr auto has_cmp = set_has_default_cmp<_Tp>;
+
+    static constexpr auto has_not = set_has_default_not<_Tp>;
+    static constexpr auto has_eq  = set_has_default_eq<_Tp>;
+
+public:
+
+    static constexpr auto op_and(const _Tp &a, const _Tp &b) -> decltype(auto)
+        requires(has_sub || (has_or && (has_xor || has_not)))
+    {
+        if constexpr (has_and) {
+            return a & b;
+        } else if constexpr (has_sub) {
+            return a / (a / b);
+        } else if constexpr (has_or && has_xor) {
+            return (a | b) ^ (a ^ b);
+        } else if constexpr (has_or && has_not) {
+            return ~((~a) | (~b));
+        } else {
+            static_assert(false);
+        }
+    }
+
+    static constexpr auto op_or(const _Tp &a, const _Tp &b) -> decltype(auto)
+        requires((has_and || has_sub) && (has_xor || has_not))
+    {
+        if constexpr (has_or) {
+            return a | b;
+        } else if constexpr (has_sub && has_xor) {
+            return (a / b) ^ b;
+        } else if constexpr (has_and && has_xor) {
+            return (a & b) ^ (a ^ b);
+        } else if constexpr (has_sub && has_not) {
+            return ~((~a) / b);
+        } else if constexpr (has_and && has_not) {
+            return ~((~a) & (~b));
+        } else {
+            static_assert(false);
+        }
+    }
+
+    static constexpr auto op_xor(const _Tp &a, const _Tp &b) -> decltype(auto)
+        requires((has_not && (has_sub || has_or || has_and)) || (has_sub && has_or))
+    {
+        if constexpr (has_xor) {
+            return a ^ b;
+        } else if constexpr (has_sub && has_or) {
+            return (a / b) | (b / a);
+        } else if constexpr (has_not && has_sub) {
+            const auto tmp = ~b;
+            return (~(tmp / a)) / (a / tmp);
+        } else if constexpr (has_not && has_or) {
+            return (~(a | ~b)) | (~(~a | b));
+        } else if constexpr (has_not && has_and) {
+            return (~(a & b)) & (~((~a) & (~b)));
+        } else {
+            static_assert(false);
+        }
+    }
+
+    static constexpr auto op_sub(const _Tp &a, const _Tp &b) -> decltype(auto)
+        requires((has_and || has_or) && (has_xor || has_not))
+    {
+        if constexpr (has_sub) {
+            return a / b;
+        } else if constexpr (has_and && has_xor) {
+            return (a & b) ^ a;
+        } else if constexpr (has_or && has_xor) {
+            return (a | b) ^ b;
+        } else if constexpr (has_and && has_not) {
+            return a & (~b);
+        } else if constexpr (has_or && has_not) {
+            return ~(~a | b);
+        } else {
+            static_assert(false);
+        }
+    }
+
+    static constexpr auto op_cmp(const _Tp &a, const _Tp &b) -> std::partial_ordering
+        requires(
+            has_eq && (has_and || has_or || (has_sub && std::constructible_from<_Tp>))
+        )
+    {
+        if constexpr (has_cmp) {
+            return a <=> b;
+        } else {
+            const auto [pred_le, pred_ge] = [&a, &b] {
+                struct Pred {
+                    bool le, ge;
+                };
+                if constexpr (has_and) {
+                    const auto set_intersect = a & b;
+                    return Pred{a == set_intersect, b == set_intersect};
+                } else if constexpr (has_or) {
+                    const auto set_union = a | b;
+                    return Pred{b == set_union, a == set_union};
+                } else {
+                    const auto empty = _Tp{};
+                    return Pred{a / b == empty, b / a == empty};
+                }
+            }();
+
+            if (pred_ge) {
+                return pred_le ? std::partial_ordering::equivalent
+                               : std::partial_ordering::greater;
+            } else {
+                return pred_le ? std::partial_ordering::less
+                               : std::partial_ordering::unordered;
+            }
+        }
+    }
+};
+
+template <typename _Tp>
+concept set_generate_and =
+    set_like<_Tp> && !set_has_default_and<_Tp> && requires { set_op_impl<_Tp>::op_and; };
+
+template <typename _Tp>
+concept set_generate_or =
+    set_like<_Tp> && !set_has_default_or<_Tp> && requires { set_op_impl<_Tp>::op_or; };
+
+template <typename _Tp>
+concept set_generate_xor =
+    set_like<_Tp> && !set_has_default_xor<_Tp> && requires { set_op_impl<_Tp>::op_xor; };
+
+template <typename _Tp>
+concept set_generate_sub =
+    set_like<_Tp> && !set_has_default_sub<_Tp> && requires { set_op_impl<_Tp>::op_sub; };
+
+template <typename _Tp>
+concept set_generate_cmp =
+    set_like<_Tp> && !set_has_default_cmp<_Tp> && requires { set_op_impl<_Tp>::op_cmp; };
+
+} // namespace __detail
+
+template <typename _Tp>
+concept basic_set_like = set_like<_Tp> && requires(_Tp a) {
+    { a &a } -> std::same_as<_Tp>;
+    { a | a } -> std::same_as<_Tp>;
+    { a ^ a } -> std::same_as<_Tp>;
+};
+
+template <typename _Tp>
+concept full_set_like = set_like<_Tp> && std::constructible_from<_Tp> && requires(_Tp a) {
+    { ~a } -> std::same_as<_Tp>;
+};
+
+template <__detail::set_generate_and _Tp>
+inline constexpr auto operator&(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    return __detail::set_op_impl<_Tp>::op_and(a, b);
+}
+
+template <__detail::set_generate_or _Tp>
+inline constexpr auto operator|(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    return __detail::set_op_impl<_Tp>::op_or(a, b);
+}
+
+template <__detail::set_generate_xor _Tp>
+inline constexpr auto operator^(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    return __detail::set_op_impl<_Tp>::op_xor(a, b);
+}
+
+template <__detail::set_generate_sub _Tp>
+inline constexpr auto operator/(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    return __detail::set_op_impl<_Tp>::op_sub(a, b);
+}
+
+template <std::constructible_from<> _Tp>
+inline constexpr auto empty_set() -> _Tp {
+    return _Tp{};
+}
+
+template <full_set_like _Tp>
+inline constexpr auto full_set() -> _Tp {
+    return ~_Tp{};
+}
+
+template <basic_set_like _Tp>
+    requires(std::constructible_from<_Tp> && std::equality_comparable<_Tp>)
+inline constexpr auto operator+(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    if ((a & b) != empty_set<_Tp>())
+        throw std::invalid_argument{"Sets must be disjoint"};
+    return a | b;
+}
+
+template <basic_set_like _Tp>
+    requires(std::constructible_from<_Tp> && std::equality_comparable<_Tp>)
+inline constexpr auto operator-(const _Tp &a, const _Tp &b) -> decltype(auto) {
+    if ((a & b) != b)
+        throw std::invalid_argument{"Sets must be disjoint"};
+    return a / b;
+}
+
+template <__detail::set_generate_cmp _Tp>
+inline constexpr auto operator<=>(const _Tp &a, const _Tp &b) -> std::partial_ordering {
+    return __detail::set_op_impl<_Tp>::op_cmp(a, b);
+}
+
+} // namespace set_operation
