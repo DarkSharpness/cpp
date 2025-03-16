c8aa252595281dba94a5de9b14a1a440b90b491b
DarkSharpness <2040703891@qq.com>
2025-03-16 22:46:29 +0800

    updated

--- misc/shared_map_2.cpp
+++ misc/shared_map_2.cpp
@@ -3,14 +3,14 @@
 #include <future>
 #include <iostream>
 #include <list>
-#include <mutex>
 #include <shared_mutex>
-#include <type_traits>
 #include <unordered_map>
+#include <utility>
 #include <variant>
 
-namespace dark {
+namespace details {
 
+// since C++17 don't have std::type_identity, we implement it here
 template <typename T>
 struct type_identity {
     using type = T;
@@ -20,13 +20,13 @@ template <typename T>
 using type_identity_t = typename type_identity<T>::type;
 
 template <typename Key, typename Value>
-struct base_map {
-    std::unordered_map<Key, Value> map_;
-    std::shared_mutex mutex_;
+struct LockedMap {
+    std::unordered_map<Key, Value> map;
+    std::shared_mutex mutex;
 };
 
 template <typename Value, typename... Keys>
-class cache_impl {
+class ThreadSafeCacheImpl {
 private:
     struct LRUnode;
     using KeySet      = std::variant<const Keys *...>;
@@ -34,147 +34,190 @@ private:
     using LRUiterator = typename LRUlist::iterator;
 
     struct Entry {
-        std::shared_future<Value> future;
+    public:
+        Entry()                         = default;
+        Entry(const Value &)            = delete;
+        Entry &operator=(const Value &) = delete;
+
+    private:
+        Value value;
         LRUiterator iterator;
-    };
-    struct LRUnode {
-        KeySet key;
-        const Entry &entry;
-        LRUnode(KeySet k, const Entry &e) : key{k}, entry{e} {}
+        friend class ThreadSafeCacheImpl;
     };
 
-    template <typename Key>
-    using EntryMap = base_map<Key, Entry>;
+    struct LRUnode {
+        KeySet key;
+        Entry &entry;
+        LRUnode(KeySet k, Entry &e) : key{k}, entry{e} {}
+    };
 
 protected:
     // do not hold lock when calling any of these functions
 
     template <typename Key>
-    auto get_cache() -> EntryMap<Key> & {
-        return std::get<EntryMap<Key>>(storage);
+    auto get_cache_lock() -> LockedMap<Key, Entry> & {
+        return std::get<LockedMap<Key, Entry>>(storage);
     }
 
-    auto lru_visit(LRUiterator it) -> void {
+    auto lru_visit(Entry &entry) -> const Value & {
         const auto guard = std::lock_guard{lru_mutex};
-        lru_list.splice(lru_list.begin(), lru_list, it);
+        /** \attention The dereference of the iterator must be done within the lock */
+        const auto iterator = entry.iterator;
+        if (iterator != LRUiterator{})
+            lru_list.splice(lru_list.end(), lru_list, iterator);
+        return entry.value;
     }
 
-    template <typename Pair>
-    auto lru_emplace(const Pair &pair) -> LRUiterator {
+    template <typename Key>
+    auto lru_init(std::pair<const Key, Entry> &pair, const Value &init) -> void {
         const auto guard   = std::lock_guard{lru_mutex};
-        auto &[key, value] = pair;
-        return lru_list.emplace(lru_list.begin(), &key, value);
+        auto &[key, entry] = pair;
+        /** \attention This must be done in the lock to ensure the iterator is valid */
+        entry.value    = init;
+        entry.iterator = lru_list.emplace(lru_list.end(), KeySet{&key}, entry);
     }
 
-    template <typename Predicate, typename Policy>
-    auto lru_evict(const Predicate &pred, const Policy &policy) -> void {
-        if (!pred()) // short circuit if the predicate is false
+    template <typename Predicate, typename Evict>
+    auto lru_evict(const Predicate &predicate, const Evict &evict) -> void {
+        if (!predicate()) // short circuit if the predicate is false
             return;
-        auto lock = std::unique_lock{lru_mutex};
-        do {
-            auto [keyset, value] = lru_list.back();
-            lru_list.pop_back();
+
+        // only hold the lru lock when doing the eviction
+        auto free_list = [&]() -> LRUlist {
+            const auto guard = std::lock_guard{lru_mutex};
+            auto iter        = lru_list.begin();
+            do {
+                auto &[_, entry] = *(iter++);
+                entry.iterator   = LRUiterator{};
+                evict(entry.value);
+            } while (predicate());
+            auto result = LRUlist{};
+            result.splice(result.begin(), lru_list, lru_list.begin(), iter);
+            return result;
+        }();
+
+        // free the memory of the map outside the lock
+        for (auto &[keyset, _] : free_list) {
             std::visit(
-                [&](auto key) {
+                [&](auto *key) {
                     using Key_t        = std::decay_t<decltype(*key)>;
-                    auto &[map, mutex] = get_cache<Key_t>();
+                    auto &[map, mutex] = get_cache_lock<Key_t>();
                     const auto guard   = std::lock_guard{mutex};
                     map.erase(*key);
                 },
                 keyset
             );
-            policy(value);
-        } while (pred());
+        }
     }
 
 private:
-    std::tuple<EntryMap<Keys>...> storage;
+    std::tuple<LockedMap<Keys, Entry>...> storage;
     LRUlist lru_list;
     std::mutex lru_mutex;
 };
 
 template <typename Value>
-struct sized_value {
+struct SizedValue {
     Value value;
     std::size_t size;
     template <typename Fn>
-    sized_value(Value value, const Fn &size_fn) : value{value}, size{size_fn(value)} {}
+    SizedValue(Value value, const Fn &size_fn) : value{value}, size{size_fn(value)} {}
 };
 
+template <typename Value, typename... Keys>
+using ThreadSafeCacheSized =
+    ThreadSafeCacheImpl<std::shared_future<SizedValue<Value>>, Keys...>;
+
+} // namespace details
+
 template <typename Policy, typename Value, typename... Keys>
-class multicache : private Policy, cache_impl<sized_value<Value>, Keys...> {
+class ThreadSafeCacheSized2 : Policy, details::ThreadSafeCacheSized<Value, Keys...> {
 private:
-    using Impl = cache_impl<sized_value<Value>, Keys...>;
+    using Impl   = details::ThreadSafeCacheSized<Value, Keys...>;
+    using Sized  = details::SizedValue<Value>;
+    using Future = std::shared_future<Sized>;
 
 public:
     using Policy::Policy;
 
     template <typename Key>
-    auto Get(const type_identity_t<Key> &key) -> Value {
-        auto &[map, mutex] = Impl::template get_cache<Key>();
+    auto Get(const details::type_identity_t<Key> &key) -> Value {
+        auto future = GetFuture<Key>(key);
+        Pop();
+        return future.get().value;
+    }
+
+private:
+    template <typename Key>
+    auto GetFuture(const details::type_identity_t<Key> &key) -> Future {
+        auto &[map, mutex] = Impl::template get_cache_lock<Key>();
+
         {
             auto lock = std::shared_lock{mutex};
             if (auto it = map.find(key); it != map.end()) {
-                // copy a new future and iterator
-                auto [future, iterator] = it->second;
-                lock.unlock();
-                Impl::lru_visit(iterator);
-                return future.get().value;
+                /** \attention We must hold the reference here */
+                Impl::lru_visit(it->second);
             }
         }
 
-        auto task   = std::packaged_task<sized_value<Value>()>{[&] {
-            return sized_value<Value>{
-                Policy::template compute<Key>(key),
-                [&](const Value &value) { return Policy::size(value); }
+        auto task   = std::packaged_task<Sized()>{[this, &key] {
+            return Sized{
+                Policy::template compute<Key>(key), // compute the value
+                [&](const Value &value) {
+                    auto size = Policy::size(value);
+                    current_size_ += size;
+                    return size;
+                }
             };
         }};
         auto future = task.get_future().share();
 
         {
-            auto lock            = std::unique_lock{mutex};
-            auto [iter, success] = map.try_emplace(key);
-            if (!success) { // some other thread has inserted the key
-                auto [future, iterator] = iter->second;
-                lock.unlock();
-                Impl::lru_visit(iterator);
-                return future.get().value;
-            }
-            // insert a new future and iterator
-            iter->second.future   = future;
-            iter->second.iterator = Impl::lru_emplace(*iter);
+            auto lock          = std::unique_lock{mutex};
+            auto [it, success] = map.try_emplace(key);
+            if (!success) // some other thread has inserted the key
+                return Impl::lru_visit(it->second);
+            Impl::lru_init(*it, future);
         }
 
         task();
-        return future.get().value;
+        return future;
     }
+
+    auto Pop() -> void {
+        Impl::lru_evict(
+            [this] { return Policy::should_evict(current_size_); },
+            [this](const Future &value) {
+                std::cout << std::format("evict {}\n", value.get().value);
+                current_size_ -= value.get().size;
+            }
+        );
+    }
+
+private:
+    std::atomic_size_t current_size_{0};
 };
 
-} // namespace dark
-
 struct Policy {
-    template <typename T>
-    auto compute(T key) -> double {
-        if constexpr (std::is_same_v<T, int>) {
-            std::cout << std::format("compute int {}\n", key);
-            return key * 2;
-        } else if constexpr (std::is_same_v<T, float>) {
-            std::cout << std::format("compute float {}\n", key);
-            return key / 2;
-        } else {
-            static_assert(false);
-            return 0;
-        }
+    auto should_evict(std::size_t size) const -> bool {
+        return size > 2;
     }
-    auto size(int value) -> std::size_t {
-        return sizeof(value);
+    auto size(const double &) const -> std::size_t {
+        return 1;
+    }
+    template <typename Key>
+    auto compute(const Key &k) const -> double {
+        std::cout << std::format("compute {}\n", k);
+        return k * sizeof(Key);
     }
 };
 
 auto main() -> int {
-    dark::multicache<Policy, double, int, float> cache;
-    std::cout << std::format("{}\n", cache.Get<int>(1));
-    std::cout << std::format("{}\n", cache.Get<float>(1.0));
-    std::cout << std::format("{}\n", cache.Get<int>(1));
-    std::cout << std::format("{}\n", sizeof(cache));
+    ThreadSafeCacheSized2<Policy, double, int, long long> cache;
+    std::cout << cache.Get<int>(1) << '\n';
+    std::cout << cache.Get<long long>(2) << '\n';
+    std::cout << cache.Get<long long>(1) << '\n';
+    std::cout << cache.Get<long long>(2) << '\n';
+    std::cout << cache.Get<int>(1) << '\n';
+    return 0;
 }
